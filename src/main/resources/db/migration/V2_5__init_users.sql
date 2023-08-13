2199c26d5a78c56a2aea398a3ea36fc681f93e01
Remik <remigiuszszejna@gmail.com>
2023-08-13 18:53:37 +0200

    Fixed init query to save clinic_user_id from update id

--- src/main/resources/db/migration/V2_5__init_users.sql
+++ src/main/resources/db/migration/V2_5__init_users.sql
@@ -10,13 +10,13 @@ insert into clinic_user (id, username, email, password, active) values (7, 'pati
 
 insert into clinic_user (id, username, email, password, active) values (8, 'test_user', 'test_user@email.pl', '$2a$12$TwQsp1IusXTDl7LwZqL0qeu49Ypr6vRdEzRq2vAsgb.zvOtrnzm5G', true);
 
-UPDATE doctor SET id = 1 WHERE pesel = '12345678912';
-UPDATE doctor SET id = 2 WHERE pesel = '12345678913';
-UPDATE doctor SET id = 3 WHERE pesel = '22345678913';
+UPDATE doctor SET clinic_user_id = 1 WHERE pesel = '12345678912';
+UPDATE doctor SET clinic_user_id = 2 WHERE pesel = '12345678913';
+UPDATE doctor SET clinic_user_id = 3 WHERE pesel = '22345678913';
 
-UPDATE patient SET id = 5 WHERE pesel = '12345678412';
-UPDATE patient SET id = 6 WHERE pesel = '12345672913';
-UPDATE patient SET id = 7 WHERE pesel = '22345600913';
+UPDATE patient SET clinic_user_id = 5 WHERE pesel = '12345678412';
+UPDATE patient SET clinic_user_id = 6 WHERE pesel = '12345672913';
+UPDATE patient SET clinic_user_id = 7 WHERE pesel = '22345600913';
 
 insert into role (id, role) values (1, 'DOCTOR'), (2, 'PATIENT'), (3, 'REST_API');
 
