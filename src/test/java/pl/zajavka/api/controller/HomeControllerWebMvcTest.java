06e312a52148ec3e92c78cd2f1cb0ebf14c87f0f
Remik <remigiuszszejna@gmail.com>
2023-08-20 16:01:39 +0200

    Added WebMvcTest tests for HomeController.java

---
+++ src/test/java/pl/zajavka/api/controller/HomeControllerWebMvcTest.java
@@ -0,0 +1,27 @@
+package pl.zajavka.api.controller;
+
+import lombok.AllArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(controllers = HomeController.class)
+@AutoConfigureMockMvc(addFilters = false)
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+public class HomeControllerWebMvcTest {
+    private MockMvc mockMvc;
+
+    @Test
+    public void homePageCanShowViewCorrectly() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"));
+    }
+}
+
